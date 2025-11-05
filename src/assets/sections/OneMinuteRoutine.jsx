bc65525b951109aba8456f0b2caddca196180623
Medel Domingo <56300107+MedelDomingo@users.noreply.github.com>
2025-11-05 20:39:32 +0800

    code updates (#3)
    Adding slick slider and correct the title

--- src/assets/sections/OneMinuteRoutine.jsx
+++ src/assets/sections/OneMinuteRoutine.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import "./OneMinuteRoutine.scss";
 
 import backgroundImage from "../images/one-minute.jpg";
 import checkedMark from "../images/check.png";
+
+const slideSettings = {
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 
 const OneMinuteRoutine = () => {
   return (
@@ -18,26 +54,48 @@ const OneMinuteRoutine = () => {
       </section>
       <div className="one-minute-list__wrapper">
         <ul>
-          <li>
-            <img src={checkedMark} alt="check-mark" />
-            No prescription Required
-          </li>
-          <li>
-            <img src={checkedMark} alt="check-mark" />
-            All-natural ingredients
-          </li>
-          <li>
-            <img src={checkedMark} alt="check-mark" />
-            Formulated by doctors & PHDs
-          </li>
-          <li>
-            <img src={checkedMark} alt="check-mark" />
-            100% risk-free returns
-          </li>
-          <li>
-            <img src={checkedMark} alt="check-mark" />
-            No prescription Required
-          </li>
+          <Slider {...slideSettings}>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              No prescription Required
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              All-natural ingredients
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              Formulated by doctors & PHDs
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              100% risk-free returns
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              No prescription Required
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              No prescription Required
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              All-natural ingredients
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              Formulated by doctors & PHDs
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              100% risk-free returns
+            </li>
+            <li>
+              <img src={checkedMark} alt="check-mark" />
+              No prescription Required
+            </li>
+          </Slider>
         </ul>
       </div>
     </>
